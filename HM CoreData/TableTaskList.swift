c402d2d0b3c11cefab41f4350bb7325e0eff915f
Tatarax <dinarvaf@icloud.com>
2023-04-09 22:46:44 +0300

    HM CoreData

--- HM CoreData/TableTaskList.swift
+++ HM CoreData/TableTaskList.swift
@@ -9,14 +9,50 @@ import UIKit
 
 class TableTaskList: UITableViewController {
  
+    //MARK: - Private properties
     private var taskList: [Task] = []
     private let cellId = "cellTaskList"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+        fetchData()
     }
 
+    private func setupView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Navigation bar appearance
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.backgroundColor = UIColor(
+            displayP3Red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 194/255
+        )
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        // Add button to navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         taskList.count
@@ -32,16 +68,21 @@ class TableTaskList: UITableViewController {
         return cell
     }
     
+    @objc private func addTask() {
+        showAlert("New Task", message: "What?")
+    }
+    
     // MARK: - Delete Task
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
-            taskList.remove(at: indexPath.row)
+            let task = taskList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            StorageManager.shared.deleteTask(task)
         }
     }
     
-    
+    // MARK: - Fetch Data
     private func fetchData(){
         StorageManager.shared.fetchData { result in
             switch result {
@@ -60,22 +101,19 @@ class TableTaskList: UITableViewController {
             
             let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
             tableView.insertRows(at: [cellIndex], with: .automatic)
-            
         }
-        
     }
     
     //MARK: - Update Task
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let task = taskList[indexPath.row]
-        showUpdateAlert()
+        showUpdateAlert("Update task", "Ok, What?")
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
     
-    // MARK: - ShowAlert
-    private func showAlert(_ message: String, _ title: String) {
+    // MARK: - ShowAlerts
+    private func showAlert(_ title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .destructive) { _ in
@@ -83,7 +121,7 @@ class TableTaskList: UITableViewController {
             self.save(task)
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
         
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
@@ -94,11 +132,26 @@ class TableTaskList: UITableViewController {
         present(alert, animated: true)
     }
     
-    
+    // Думаю ошибка тут. мне тут нужно взять уже сохраненной значение и поменять его на новое но не могу догадаться как это сделать.
     private func showUpdateAlert(_ message: String, _ title: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
+        let updateAction = UIAlertAction(title: "Save", style: .destructive) { _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else {return}
+          
+         //   StorageManager.shared.update(<#Task#>,  task)
+            self.save(task)
+        }
         
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        alert.addAction(updateAction)
+        alert.addAction(cancelAction)
+        alert.addTextField() { textField in
+            textField.placeholder = "Update Task"
+        }
+        
+        present(alert, animated: true)
     }
     
     
